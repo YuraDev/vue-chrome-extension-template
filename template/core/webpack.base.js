dfcc963ac649851fc192af8910403ad573f540c8
Luke Thompson <lpthompson92@gmail.com>
2018-10-23 11:09:12 +1100

    Fix resolve alias '@' path
    resolve('src') does not work

--- template/core/webpack.base.js
+++ template/core/webpack.base.js
@@ -27,7 +27,7 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src')
+      '@': path.join(__dirname, '..', 'src')
     }
   },
   module: {
